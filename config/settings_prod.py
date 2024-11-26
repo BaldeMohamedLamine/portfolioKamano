0e62f67395309b06df49472df6dd5a685c9fea8c
Lenz Balde <mohamedlaminebalde392@gmail.com>
2024-11-26 11:02:18 +0000

    update database config

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
 from .settings import *
+import dj_database_url 
+import environ
+import os
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
-SECRET_KEY = "6B59CB94F8B4F74BC1868B61AF8F4"
+SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.1.198",'localhost']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+DATABASES = {'defalt': dj_database_url.config(conn_health_checks=True)}
